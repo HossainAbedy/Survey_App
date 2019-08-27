cbcca8be798efd3d77d96b4aed5fa0ad061a9e74
HossainAbedy <abedy.ewu@gmail.com>
2019-08-27 16:26:28 +0600

    Test

--- public/js/app.js
+++ public/js/app.js
@@ -75988,7 +75988,7 @@ var staticRenderFns = [
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
               _vm._v(
-                "\n                    I'm an Dashboard component.\n                "
+                "\n                    I'm an Dashboard component.(Under Development)\n                "
               )
             ])
           ])
